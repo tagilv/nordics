5fa0a93ca8042260416530286c429f27f4a7668c
Viktor Tagil <viktor.tagil@gmail.com>
2025-09-07 17:46:01 +0200

    Fix ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    ignores: [
+      "lib/generated/**/*",
+      "prisma/migrations/**/*",
+      "node_modules/**/*",
+    ],
+  },
 ];
 
 export default eslintConfig;
